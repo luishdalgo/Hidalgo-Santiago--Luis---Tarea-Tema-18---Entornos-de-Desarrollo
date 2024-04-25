1f6339cbd40963ad59e5764c44f6d662566604ce
luishdalgo <lhs0003@alu.medac.es>
2024-04-25 15:59:22 +0200

    finaldevelop

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, release!");
+        System.out.println("Hello, finaldevelop!");
     }
 }
